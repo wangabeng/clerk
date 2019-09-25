65376ba5726c4e8519d6ac11b1a815f9905bc3db
wangabeng <wangabeng@163.com>
2019-09-26 01:38:40 +0800

    upload

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,5 @@
 module.exports = {
-  // presets: [
-  //   '@vue/app'
-  // ]
-  "presets": [["es2015", { "modules": false }]],
-  "plugins": [
-    [
-      "component",
-      {
-        "libraryName": "element-ui",
-        "styleLibraryName": "theme-chalk"
-      }
-    ]
+  presets: [
+    '@vue/app'
   ]
 }
